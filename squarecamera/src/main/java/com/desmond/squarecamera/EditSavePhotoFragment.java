84d79d071051a6b8d3b43f1818742b2e31cc301c
desmond <desmond@replaid.com>
2015-10-12 01:01:50 +0800

    Add multiple permission request in RuntimePermissionActivity Get result from activity in fragment

--- squarecamera/src/main/java/com/desmond/squarecamera/EditSavePhotoFragment.java
+++ squarecamera/src/main/java/com/desmond/squarecamera/EditSavePhotoFragment.java
@@ -29,7 +29,7 @@ public class EditSavePhotoFragment extends Fragment {
     public static final String ROTATION_KEY = "rotation";
     public static final String IMAGE_INFO = "image_info";
 
-    private static final int REQUEST_STORAGE = 1;
+    private static final int REQUEST_STORAGE = 10;
 
     public static Fragment newInstance(byte[] bitmapByteArray, int rotation,
                                        @NonNull ImageParameters parameters) {
@@ -110,7 +110,7 @@ public class EditSavePhotoFragment extends Fragment {
     }
 
     private void requestForPermission() {
-        RuntimePermissionActivity.startActivity(getActivity(),
+        RuntimePermissionActivity.startActivity(EditSavePhotoFragment.this,
                 REQUEST_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE);
     }
--- squarecamera/src/main/java/com/desmond/squarecamera/RuntimePermissionActivity.java
+++ squarecamera/src/main/java/com/desmond/squarecamera/RuntimePermissionActivity.java
@@ -7,24 +7,38 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RuntimePermissionActivity extends AppCompatActivity {
 
     public static final String REQUESTED_PERMISSION = "requested_permission";
     private static final int REQUEST_CODE = 1;
 
-    public static void startActivity(final FragmentActivity activity,
+    public static void startActivity(@NonNull final Fragment fragment,
                                      final int requestCode,
-                                     final String requestedPermission) {
-        final Intent intent = new Intent(activity, RuntimePermissionActivity.class);
-        intent.putExtra(REQUESTED_PERMISSION, requestedPermission);
-        activity.startActivityForResult(intent, requestCode);
+                                     @NonNull final String requestedPermission,
+                                     final String... permissions) {
+
+        final Intent intent = new Intent(fragment.getActivity(), RuntimePermissionActivity.class);
+
+        final int capacity = 1 + (permissions != null ? permissions.length : 0);
+        final ArrayList<String> requestedPermissions = new ArrayList<>(capacity);
+        requestedPermissions.add(requestedPermission);
+        if (permissions != null) {
+            requestedPermissions.addAll(Arrays.asList(permissions));
+        }
+
+        intent.putStringArrayListExtra(REQUESTED_PERMISSION, requestedPermissions);
+        fragment.startActivityForResult(intent, requestCode);
     }
 
+    /* https://code.google.com/p/android-developer-preview/issues/detail?id=2353 */
     @Override
     protected void onStart() {
         super.onStart();
@@ -35,24 +49,49 @@ public class RuntimePermissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final String requestedPermission = getIntent().getStringExtra(REQUESTED_PERMISSION);
-        if (ContextCompat.checkSelfPermission(RuntimePermissionActivity.this, requestedPermission)
-                != PackageManager.PERMISSION_GRANTED) {
+        final ArrayList<String> reqPermissions = getIntent().getStringArrayListExtra(REQUESTED_PERMISSION);
+        final ArrayList<String> permissionsNeeded = getPermissionNeeded(reqPermissions);
+        final ArrayList<String> permissionRationaleNeeded = getPermissionRationaleNeeded(permissionsNeeded);
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    RuntimePermissionActivity.this, requestedPermission)) {
-
-                showPermissionRationaleDialog(
-                        getString(R.string.squarecamera__request_write_storage_permission_text),
-                        requestedPermission);
-
-            } else {
-                requestForPermission(requestedPermission);
+        if (!permissionRationaleNeeded.isEmpty()) {
+            String message = getString(R.string.squarecamera__request_write_storage_permission_text);
+            for (int i = 1; i < permissionRationaleNeeded.size(); ++i) {
+                message += ", " + permissionRationaleNeeded.get(i);
             }
 
+            showPermissionRationaleDialog(message, permissionsNeeded.toArray(new String[permissionsNeeded.size()]));
+        } else if (!permissionsNeeded.isEmpty()) {
+            requestForPermission(permissionsNeeded.toArray(new String[permissionsNeeded.size()]));
         } else {
             sendResult(true);
         }
+    }
+
+    private ArrayList<String> getPermissionNeeded(@NonNull final ArrayList<String> reqPermissions) {
+        final ArrayList<String> permissionNeeded = new ArrayList<>(reqPermissions.size());
+
+        for (String reqPermission : reqPermissions) {
+            if (ContextCompat.checkSelfPermission(RuntimePermissionActivity.this, reqPermission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                permissionNeeded.add(reqPermission);
+            }
+        }
+
+        return permissionNeeded;
+    }
+
+    private ArrayList<String> getPermissionRationaleNeeded(@NonNull final ArrayList<String> permissionsNeeded) {
+        final ArrayList<String> rationaleNeeded = new ArrayList<>(permissionsNeeded.size());
+
+        for (String permissionNeeded : permissionsNeeded) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    RuntimePermissionActivity.this, permissionNeeded)) {
+                rationaleNeeded.add(permissionNeeded);
+            }
+        }
+
+        return rationaleNeeded;
     }
 
     @Override
@@ -75,13 +114,13 @@ public class RuntimePermissionActivity extends AppCompatActivity {
         }
     }
 
-    private void showPermissionRationaleDialog(final String message, final String permission) {
+    private void showPermissionRationaleDialog(final String message, final String[] permissions) {
         new AlertDialog.Builder(RuntimePermissionActivity.this)
                 .setMessage(message)
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        RuntimePermissionActivity.this.requestForPermission(permission);
+                        RuntimePermissionActivity.this.requestForPermission(permissions);
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -106,8 +145,8 @@ public class RuntimePermissionActivity extends AppCompatActivity {
                 .show();
     }
 
-    private void requestForPermission(final String permission) {
-        ActivityCompat.requestPermissions(RuntimePermissionActivity.this, new String[] {permission}, REQUEST_CODE);
+    private void requestForPermission(final String[] permissions) {
+        ActivityCompat.requestPermissions(RuntimePermissionActivity.this, permissions, REQUEST_CODE);
     }
 
     private void sendResult(final boolean isPermissionGranted) {
